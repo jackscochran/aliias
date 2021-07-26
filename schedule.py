c956ff2d4f972524c7877b1b993a24174370e3fa
Jack Cochran <64762170+jackscochran@users.noreply.github.com>
2021-07-26 17:03:41 -0600

    Changed scheduled extraction date

--- schedule.py
+++ schedule.py
@@ -2,5 +2,5 @@ import controllers.data_pipeline as data_pipeline
 import datetime
 
 # collect data on all the companies who released their earnings yesterday
-data_pipeline.collect_earnings(str(datetime.date.today() - datetime.timedelta(days=1)))
-
+# data_pipeline.collect_earnings(str(datetime.date.today() - datetime.timedelta(days=1)))
+data_pipeline.collect_earnings('2021-07-23')
