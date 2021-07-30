d4a01c028be32ed12e6564b86e97926ec79a8926
Jack Cochran <64762170+jackscochran@users.noreply.github.com>
2021-07-30 14:30:11 -0600

    Added command file

--- schedule.py
+++
@@ -1,6 +0,0 @@
-import controllers.data_pipeline as data_pipeline
-import datetime
-
-# collect data on all the companies who released their earnings yesterday
-data_pipeline.collect_earnings(str(datetime.date.today() - datetime.timedelta(days=1)))
-
