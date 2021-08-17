48af371b90d754fb4a80a8475a8eab93d7c4c0d7
Jack Cochran <64762170+jackscochran@users.noreply.github.com>
2021-08-16 20:01:30 -0600

    Testing route view file locations

--- app.py
+++ app.py
@@ -4,16 +4,22 @@ import flask
 app = flask.Flask(__name__)
 
 # after creating app, import all route files
-import routes.index
-import routes.about
-import routes.account
-import routes.company
-import routes.help
-import routes.policies
-import routes.portfolio
-import routes.pricing
-import routes.search
-import routes.user_auth
+# import routes.index
+# import routes.about
+# import routes.account
+# import routes.company
+# import routes.help
+# import routes.policies
+# import routes.portfolio
+# import routes.pricing
+# import routes.search
+# import routes.user_auth
+
+
+@app.route('/')
+def index():
+    # home page
+    return flask.render_template('index.html')
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
